3b99922826edbeaf82e115a653b7e7cc49815ae0
Foxi224 <118421219+Foxi224@users.noreply.github.com>
2022-11-22 13:30:46 +0900

    Add files via upload

---
+++ catch/catch/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace cart18
+{ 
+    class Program
+{
+    static void Main(string[] args)
+    {
+            try
+            {
+                int x = 5;
+                int y = x * 5;
+                Console.WriteLine($"Решение= {y}");
+            }
+            catch
+            {
+                Console.WriteLine("Возникло исключение!");
+            
+            
+            }
+    }
+}
+}
